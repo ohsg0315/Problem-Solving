da20d3c04656bb5af82b4d24785930132f1809eb
Seong <ohsg0315@gmail.com>
2020-07-21 23:19:58 +0900

    BOJ (10871) - X보다 작은 수

---
+++ Baekjoon Online Judge/BOJ (10871) - X보다 작은 수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int[] cache;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(tokenizer.nextToken()), X = Integer.parseInt(tokenizer.nextToken());
+        tokenizer = new StringTokenizer(br.readLine(), " ");
+
+        while (tokenizer.hasMoreTokens()) {
+            int bufNumber = Integer.parseInt(tokenizer.nextToken());
+            if (bufNumber < X) {
+                sb.append(bufNumber + " ");
+            }
+        }
+        System.out.println(sb.toString().toString());
+    } // end of main
+} // end of class
