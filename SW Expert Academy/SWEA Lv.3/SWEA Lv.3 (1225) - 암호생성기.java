d1ad48045a405f3de3cf69da5e1aef8d813ec105
Seong <ohsg0315@gmail.com>
2020-07-30 17:30:41 +0900

    SWEA Lv.3 (1225) - 암호생성기

--- SW Expert Academy/SWEA Lv.3/SWEA Lv.3 (1225) - 암호생성기.java
+++ SW Expert Academy/SWEA Lv.3/SWEA Lv.3 (1225) - 암호생성기.java
@@ -2,8 +2,6 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    private static boolean[] flag;
-
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer;
