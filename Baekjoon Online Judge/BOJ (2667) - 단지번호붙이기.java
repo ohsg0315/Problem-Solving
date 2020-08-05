b28367485c7fc74216a4af8c85851d6aa42a39d5
ohsg0315 <44195636+ohsg0315@users.noreply.github.com>
2020-08-05 16:27:10 +0900

    Update BOJ (2667) - 단지번호붙이기.java

--- Baekjoon Online Judge/BOJ (2667) - 단지번호붙이기.java
+++ Baekjoon Online Judge/BOJ (2667) - 단지번호붙이기.java
@@ -1,78 +1,80 @@
 import java.util.*;
+import java.awt.Point;
 import java.io.*;
 
 public class Main {
-    private static int[][] map;
-    private static int N;
-    private static int[] dx = {1, -1, 0, 0};
-    private static int[] dy = {0, 0, 1, -1};
-    private static boolean[][] visited;
-    private static ArrayList<Integer> villageCnt = new ArrayList<>();
+	private static int N, cnt;
+	private static int[] res;
+	private static int[] dx = { 0, 0, -1, 1 };
+	private static int[] dy = { -1, 1, 0, 0 };
+	private static int[][] village;
+	private static boolean[][] visited;
 
-    private static class Point {
-        int x, y;
+	private static boolean isRange(int x, int y) {
+		if (x < 0 || y < 0 || x >= N || y >= N) {
+			return false;
+		}
+		return true;
+	}
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	private static int bfs(int x, int y) {
+		int res = 0;
+		Queue<Point> qu = new LinkedList<>();
 
-    private static boolean isRange(int x, int y) {
-        if (x < 0 || y < 0 || x >= N || y >= N) {
-            return false;
-        }
-        return true;
-    }
+		visited[y][x] = true;
+		qu.offer(new Point(x, y));
 
-    private static void BFS(int x, int y) {
-        int res = 0;
-        Queue<Point> qu = new LinkedList<>();
+		while (!qu.isEmpty()) {
+			res++;
+			Point now = qu.poll();
 
-        qu.offer(new Point(x, y));
-        visited[y][x] = true;
+			for (int i = 0; i < 4; i++) {
+				int nextX = now.x + dx[i];
+				int nextY = now.y + dy[i];
 
-        while (!qu.isEmpty()) {
-            Point now = qu.poll();
-            res++;
+				if (!isRange(nextX, nextY)) {
+					continue;
+				}
 
-            for (int i = 0; i < 4; i++) {
-                int nextX = now.x + dx[i];
-                int nextY = now.y + dy[i];
+				if (village[nextY][nextX] == 1 && !visited[nextY][nextX]) {
+					visited[nextY][nextX] = true;
+					qu.offer(new Point(nextX, nextY));
+				}
+			}
+		}
+		return res;
+	}
 
-                if (!isRange(nextX, nextY) || map[nextY][nextX] == 0 || visited[nextY][nextX]) {
-                    continue;
-                }
-                visited[nextY][nextX] = true;
-                qu.offer(new Point(nextX, nextY));
-            }
-        }
-        villageCnt.add(res);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
+		N = Integer.parseInt(br.readLine());
+		village = new int[N][N];
+		visited = new boolean[N][N];
+		res = new int[N * N];
 
-        for (int i = 0; i < N; i++) {
-            map[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] != 0 && !visited[i][j]) {
-                    BFS(j, i);
-                }
-            }
-        }
-        Collections.sort(villageCnt);
-
-        System.out.println(villageCnt.size());
-        for (int i : villageCnt) {
-            System.out.println(i);
-        }
-
-    } // end of main
-} // end of class
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < N; j++) {
+				village[i][j] = Integer.parseInt(s.charAt(j) + "");
+			}
+		}
+		// 탐색
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (village[i][j] == 1 && !visited[i][j]) {
+					res[cnt++] = bfs(j, i);
+				}
+			}
+		}
+		// 배열 크기 초기화 && 정렬 && 답 출력
+		res = Arrays.copyOf(res, cnt);
+		Arrays.sort(res);
+		StringBuilder sb = new StringBuilder();
+		sb.append(cnt).append("\n");
+		for (int i = 0; i < cnt; i++) {
+			sb.append(res[i]).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
