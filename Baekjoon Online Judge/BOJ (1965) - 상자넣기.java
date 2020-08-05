edd587280b56618cacbc24f805849412fb667c63
ohsg0315 <44195636+ohsg0315@users.noreply.github.com>
2020-08-05 17:40:25 +0900

    Update BOJ (1965) - 상자넣기.java

--- Baekjoon Online Judge/BOJ (1965) - 상자넣기.java
+++ Baekjoon Online Judge/BOJ (1965) - 상자넣기.java
@@ -2,12 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	private static int[] box;
+
+	private static int lowerBound(int num, int high) {
+		int low = 0;
+
+		while (low < high) {
+			int mid = (low + high) / 2;
+
+			if (box[mid] >= num) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return high;
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		TreeSet<Integer> ts = new TreeSet<>();
 		StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+		box = new int[N + 1];
 
+		// 배열 사용
+		int cnt = 0;
+		box[0] = Integer.parseInt(tokenizer.nextToken());
+		for (int i = 1; i < N; i++) {
+			int cur = Integer.parseInt(tokenizer.nextToken());
+
+			if (box[cnt] >= cur) {
+				int idx = lowerBound(cur, cnt);
+				box[idx] = cur;
+			} else {
+				box[++cnt] = cur;
+			}
+		}
+		System.out.println(cnt + 1);
+
+		// TreeSet 사용
+		/*
+		TreeSet<Integer> ts = new TreeSet<>();
 		ts.add(0);
 		for (int i = 0; i < N; i++) {
 			int cur = Integer.parseInt(tokenizer.nextToken());
@@ -18,5 +53,6 @@ public class Main {
 			ts.add(cur);
 		}
 		System.out.println(ts.size() - 1);
+		*/
 	}
 }
