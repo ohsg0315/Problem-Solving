c38eb0d63a66a9b6bb335da3c09fc057090babde
ohsg0315 <44195636+ohsg0315@users.noreply.github.com>
2020-08-07 15:38:12 +0900

    BOJ (10171) - 고양이

---
+++ Baekjoon Online Judge/BOJ (10171) - 고양이.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb= new StringBuilder();
+		
+		sb.append("\\    /\\\n");
+		sb.append(" )  ( ')\n");
+		sb.append("(  /  )\n");
+		sb.append(" \\(__)|\n");
+		System.out.println(sb.toString());
+	}
+}
