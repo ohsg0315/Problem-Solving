9644f810500317b37f7bd93eebfee3316e2a10e9
ohsg0315 <44195636+ohsg0315@users.noreply.github.com>
2020-08-07 15:40:35 +0900

    BOJ (10172) - 개

---
+++ Baekjoon Online Judge/BOJ (10172) - 개.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("|\\_/|").append("\n");
+		sb.append("|q p|   /}").append("\n");
+		sb.append("( 0 )\"\"\"\\").append("\n");
+		sb.append("|\"^\"`    |").append("\n");
+		sb.append("||_/=\\\\__|").append("\n");
+		System.out.println(sb.toString());
+	}
+}
