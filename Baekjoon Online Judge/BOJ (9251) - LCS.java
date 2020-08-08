fc91365569dd3460335290d29d14f8eca0153de0
Seong <ohsg0315@gmail.com>
2020-08-09 00:53:59 +0900

    BOJ (9251) - LCS

---
+++ Baekjoon Online Judge/BOJ (9251) - LCS.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int[][] lcs = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    lcs[i + 1][j + 1] = lcs[i][j] + 1;
+                } else {
+                    lcs[i + 1][j + 1] = Math.max(lcs[i + 1][j], lcs[i][j + 1]);
+                }
+            }
+        }
+        System.out.println(lcs[s1.length()][s2.length()]);
+    } // end of main
+} // end of class
