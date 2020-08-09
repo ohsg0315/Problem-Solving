7cdfce0357cc413d511ca791fb5e875630894f3d
Seong <ohsg0315@gmail.com>
2020-08-09 19:35:13 +0900

    BOJ (15650) - N과 M (2)

---
+++ Baekjoon Online Judge/BOJ (15650) - N과 M (2).java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int n, r;
+    private static int[] res;
+    private static boolean[] selected;
+    private static BufferedWriter wr;
+
+    private static void permutation(int idx, int cnt) throws IOException {
+        if (cnt == r) {
+            for (int i = 0; i < r - 1; i++) {
+                wr.write(res[i] + " ");
+            }
+            wr.write(res[r - 1] + "\n");
+            return;
+        }
+        for (int i = idx; i < n + 1; i++) {
+            res[cnt] = i;
+            permutation(i + 1, cnt + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+        wr = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(tokenizer.nextToken());
+        r = Integer.parseInt(tokenizer.nextToken());
+        res = new int[r];
+        selected = new boolean[n + 1];
+
+        permutation(1, 0);
+        wr.close();
+    } // end of main
+} // end of class
