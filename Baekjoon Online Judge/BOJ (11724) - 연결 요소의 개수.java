d35cd8c5a1ceb774a198b3254e330a21b9d69d8b
Seong <ohsg0315@gmail.com>
2020-08-10 20:42:47 +0900

    BOJ (11724) - 연결 요소의 개수

---
+++ Baekjoon Online Judge/BOJ (11724) - 연결 요소의 개수.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int[] parent;
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a > b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        HashSet<Integer> set = new HashSet<>();
+
+        parent = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(tokenizer.nextToken());
+            int b = Integer.parseInt(tokenizer.nextToken());
+
+            union(a, b);
+        }
+        for (int i = 1; i < n + 1; i++) {
+            set.add(find(parent[i]));
+        }
+
+        System.out.println(set.size());
+    } // end of main
+} // end of class
