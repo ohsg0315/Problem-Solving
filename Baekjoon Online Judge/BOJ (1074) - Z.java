1283d8ff4c47ebe1575e0ca5019e84765632136a
Seong <ohsg0315@gmail.com>
2020-08-25 16:53:06 +0900

    BOJ (1074) - Z

---
+++ Baekjoon Online Judge/BOJ (1074) - Z.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int N, r, c, cnt, res;
+    private static boolean flag;
+
+    private static void dq(int n, int x, int y) {
+        if (flag) return;
+        if (n == 1) {
+            if (r == y && c == x) {
+                res = cnt;
+                flag = true;
+            }
+            cnt++;
+            return;
+        }
+        dq(n / 2, x, y);
+        dq(n / 2, x + n / 2, y);
+        dq(n / 2, x, y + n / 2);
+        dq(n / 2, x + n / 2, y + n / 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        N = 2 << Integer.parseInt(tokenizer.nextToken());
+        r = Integer.parseInt(tokenizer.nextToken());
+        c = Integer.parseInt(tokenizer.nextToken());
+
+        dq(N, 0, 0);
+
+        System.out.println(res);
+    } // end of main
+} // end of class`
