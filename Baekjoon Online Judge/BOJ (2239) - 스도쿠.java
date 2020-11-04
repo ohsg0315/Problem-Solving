b8e8995b6451e3f42d3d8624ba68d9c22d7a8c26
ohsg0315 <44195636+ohsg0315@users.noreply.github.com>
2020-11-04 11:45:05 +0900

    BOJ (2239) - 스도쿠

---
+++ Baekjoon Online Judge/BOJ (2239) - 스도쿠.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * 
+ *  실행시간 : 632ms
+ *  
+ *  */
+public class Main {
+	private static int needs;
+	private static boolean flag;
+	private static int[][] board;
+	private static StringBuilder sb;
+
+	private static boolean isRight(int r, int c, int value) {
+
+		// 행 검사
+		for (int i = 0; i < 9; i++) {
+			if (board[r][i] == value)
+				return false;
+		}
+
+		// 열 검사
+		for (int i = 0; i < 9; i++) {
+			if (board[i][c] == value)
+				return false;
+		}
+
+		// 3 X 3 블록 검사
+		int blockR = (r / 3) * 3, blockC = (c / 3) * 3;
+		for (int i = blockR; i < blockR + 3; i++) {
+			for (int j = blockC; j < blockC + 3; j++) {
+				if (board[i][j] == value)
+					return false;
+			}
+		}
+		return true;
+	}
+
+	private static void find(int cnt, int r, int c) {
+		if (cnt == needs) {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sb.append(board[i][j]);
+				}
+				sb.append('\n');
+			}
+			flag = true;
+			return;
+		}
+
+		// 0이 아닌 수
+		if (board[r][c] != 0) {
+			if (c == 8)
+				find(cnt, r + 1, 0);
+			else
+				find(cnt, r, c + 1);
+		}
+
+		// 0 이면 수를 채운다
+		else {
+			for (int i = 1; i < 10; i++) {
+				if (isRight(r, c, i)) {
+					board[r][c] = i;
+
+					if (c == 8)
+						find(cnt + 1, r + 1, 0);
+					else
+						find(cnt + 1, r, c + 1);
+
+					board[r][c] = 0;
+				}
+
+				// 종료 플래그
+				if (flag)
+					return;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		board = new int[9][9];
+		sb = new StringBuilder();
+
+		for (int i = 0; i < 9; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < 9; j++) {
+				board[i][j] = s.charAt(j) - '0';
+
+				if (board[i][j] == 0)
+					needs++;
+			}
+		}
+		find(0, 0, 0);
+		System.out.print(sb.toString());
+	}
+}
