c7409babbe9cce2a24149acc8fbff311df3835ab
Seong <ohsg0315@gmail.com>
2020-11-26 20:00:17 +0900

    BOJ (15736) - 청기 백기

---
+++ Baekjoon Online Judge/BOJ (15736) - 청기 백기.java
@@ -0,0 +1,11 @@
+import javax.lang.model.type.PrimitiveType;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.print((int)Math.sqrt(n));
+    } // end of main
+}
