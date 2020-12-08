fe717e43f7bdfbc923faf18e05e9422eb3b3d550
Seong <ohsg0315@gmail.com>
2020-12-09 00:07:24 +0900

    BOJ (11727) - 2×n 타일링 2

---
+++ Baekjoon Online Judge/BOJ (11727) - 2×n 타일링 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static final int DIV = 10_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] cache = new int[n + 1];
+
+        cache[0] = 1;
+        cache[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            cache[i] = (cache[i - 1] + (cache[i - 2] * 2)) % DIV;
+        }
+
+        System.out.println(cache[n]);
+    } // end of main
+}
