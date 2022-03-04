c992865bee3bdb256d93eb1afd698878d6d7f0e5
Seong Oh <ohsg0315@gmail.com>
2022-03-05 00:49:53 +0900

    LeetCode (80) - Remove Duplicates from Sorted Array II

--- Leet Code/LeetCode (80) - Remove Duplicates from Sorted Array II.java
+++ Leet Code/LeetCode (80) - Remove Duplicates from Sorted Array II.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 class Solution {
+    // 4ms
     public int removeDuplicates(int[] nums) {
         boolean moreThanTwo = false;
         int before = Integer.MIN_VALUE, result = 0;
@@ -20,6 +21,34 @@ class Solution {
         }
 
         Arrays.sort(nums);
+
+        return result;
+    }
+
+    // 1ms
+    // Snowball technique
+    public int removeDuplicates(int[] nums) {
+        int before = nums[0], duplicates = 0, result = 1;
+        boolean moreThanTwo = false;
+
+        for (int i = 1, length = nums.length; i < length; i++) {
+            int cur = nums[i];
+            if (before == cur) {
+                if (moreThanTwo) {
+                    duplicates++;
+                } else {
+                    result++;
+                    nums[i - duplicates] = cur;
+                    moreThanTwo = true;
+                }
+            }else {
+                nums[i] = 0;
+                nums[i - duplicates] = cur;
+                before = cur;
+                moreThanTwo = false;
+                result++;
+            }
+        }
 
         return result;
     }
