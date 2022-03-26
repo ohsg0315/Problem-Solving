7bf1b6148cfe34995d6c265590f2f3bb89194755
Seong Oh <ohsg0315@gmail.com>
2022-03-26 19:48:12 +0900

    LeetCode (704) - Binary Search

---
+++ Leet Code/LeetCode (704) - Binary Search.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int result = Arrays.binarySearch(nums, target);
+
+        return result >= 0 ? result : -1;
+    }
+}
